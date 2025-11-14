4530bddc494fe54e95b6b39b8619929d52be3e6f
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-11-14 15:49:04 +0900

    Feat: 수파베이스  회원가입 / 로그인 구현

---
+++ src/api/auth.ts
@@ -0,0 +1,19 @@
+/** 인증관련 API 호출 */
+
+import supabase from '@/utils/supabase';
+
+export async function signUp({ email, password }: { email: string; password: string }) {
+	const { data, error } = await supabase.auth.signUp({ email, password });
+
+	if (error) throw error;
+
+	return data;
+}
+
+export async function signInWithPassword({ email, password }: { email: string; password: string }) {
+	const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+
+	if (error) throw error;
+
+	return data;
+}
