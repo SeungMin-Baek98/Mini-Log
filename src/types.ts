e0d4f920b461d19ab23ba2199adb3c5ecaaae14b
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2025-12-07 17:50:18 +0900

    Feat: 댓글 POST요청 구현 / api 및 mutation 로직 생성

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,8 @@ import { type Database } from './database.types';
 /** Supabase Post 타입 정의 */
 export type PostEntity = Database['public']['Tables']['post']['Row'];
 export type ProfileEntity = Database['public']['Tables']['profile']['Row'];
+export type CommentEntity = Database['public']['Tables']['comment']['Row'];
+
 export type Post = PostEntity & { author: ProfileEntity; isLiked: boolean };
 
 /** useMutation 콜백 타입 정의 */
