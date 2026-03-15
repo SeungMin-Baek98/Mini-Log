c442906c028faf58a46595ef4457af7f7c337954
백승민 <jah02190@naver.com>
2026-03-15 20:30:31 +0900

    📝 chore: 로고 이미지 변경 및 파비콘 추가

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -10,16 +10,18 @@ export default function Header() {
 		<header className="supports-[backdrop-filter]:bg-background/72 sticky top-0 z-50 border-b border-white/40 px-4 backdrop-blur-xl sm:px-6">
 			<div className="m-auto flex h-18 w-full max-w-5xl items-center justify-between">
 				<Link to={'/'} className="flex items-center gap-3">
-					{/* <img
-						src={logo}
-						alt="미니 로그의 로고, 메세지 말풍선을 형상화한 모양이다"
-						className="w-7 drop-shadow-[0_8px_18px_rgba(47,93,80,0.16)]"
-					/> */}
 					<div className="flex flex-col">
 						<span className="text-primary/70 text-[0.7rem] tracking-[0.28em] uppercase">
 							Warm Lifelog
 						</span>
-						<div className="text-lg font-semibold">미니 로그</div>
+						<div className="flex gap-2 text-lg font-semibold">
+							<img
+								src={logo}
+								alt="미니 로그의 로고, 메세지 말풍선을 형상화한 모양이다"
+								className="inset-0 h-8 w-8 object-cover"
+							/>
+							미니 로그
+						</div>
 					</div>
 				</Link>
 				<div className="flex items-center gap-4">
