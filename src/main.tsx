22a7168955fed3a3a87457c468fcf78c4b66c925
백승민 <jah02190@naver.com>
2026-03-20 21:26:21 +0900

    ♻️ Toaster 컴포넌트 경로 수정: 'sonner'에서 './components/ui/sonner.tsx'로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import './index.css';
 import App from './App.tsx';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from 'sonner';
+
 import { queryClient } from '@/lib/queryClient';
+import { Toaster } from './components/ui/sonner.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<QueryClientProvider client={queryClient}>
