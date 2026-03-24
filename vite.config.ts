de3188af1ee940588afe3b6457174a210983854e
백승민 <jah02190@naver.com>
2026-03-24 20:17:48 +0900

    ✨ vite.config.ts에 visualizer 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		visualizer({
+			filename: 'dist/stats.html'
+		}) as PluginOption
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src')
